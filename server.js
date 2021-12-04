90175ac31d1fce3eac2cde21c43b512d1c22ffc6
Chrisaeus <87839602+Chrisaeus@users.noreply.github.com>
2021-12-04 00:05:33 -0800

    GET and POST requests completed. Main requirements met

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 const PORT = 3001;
@@ -7,14 +9,48 @@ const PORT = 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public/notes.html'));
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/notes.html"));
 });
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public/index.html'));
+app.get("/api/notes", (req, res) => {
+  fs.readFile("db/db.json", "utf8", (err, data) =>
+    err ? console.error(err) : res.json(JSON.parse(data))
+  );
+});
+
+app.post("/api/notes", (req, res) => {
+  const { title, text } = req.body;
+
+  if (req.body) {
+    const newNote = {
+      title,
+      text,
+      id: uuidv4()
+    };
+
+    fs.readFile("db/db.json", "utf8", (err, data) => {
+      if (err) {
+        console.error(err);
+      } else {
+        const parsedData = JSON.parse(data);
+        parsedData.push(newNote);
+        fs.writeFile("db/db.json", JSON.stringify(parsedData, null, 4), (err) => 
+          err ? console.error(err) : console.info("Note saved.")
+        );
+      };
+    });
+
+    res.json("Note saved.");
+  } else {
+    res.error("Error in adding note")
+  };
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/index.html"));
 });
 
 app.listen(PORT, () => {
